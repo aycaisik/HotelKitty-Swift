0f6ad0ca1fe8a5e37e37d162eeebcb8104effbd7
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-22 01:19:01 +0300

    Update RoomType.swift

--- HotelMiuw/Models/RoomType.swift
+++ HotelMiuw/Models/RoomType.swift
@@ -20,5 +20,17 @@ struct RoomType : Equatable{
          return lhs.id == rhs.id
     }
     
+    //Static bir şey oluşturucam çünkü ben struct veya class üzerinden tüm oda tiplerine ulaşmak //istiyorum. Static yaptığımızda instance değil de struct ve class tan çekebiliriz.
+    //Static yazarsan classın yazmazsan objenin olur...
+    
+    static var all: [RoomType] {
+        return [
+            RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+            RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+            RoomType(id: 2, name: "Suit", shortName: "S", price: 309),
+        ]
+    }
+
+    //RoomType.all
     
 }
